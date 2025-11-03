21f76ed9f3ae7ccb212d9df572f64b780e616aaa
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2025-11-03 12:49:34 -0600

    fix: dependencies for neovim

--- home/packages.nix
+++ home/packages.nix
@@ -12,10 +12,14 @@
       posting # Terminal API client
 
       # programming languages
-      nodejs_20 # Includes npm
+      nodejs_22 # Includes npm
       python3 # For uv
       uv # Fast Python package installer
       gcc # Includes g++
+      luarocks
+      lua5_1 # Lua 5.1 interpreter for Neovim
+
+
 
       # misc
       yt-dlp
