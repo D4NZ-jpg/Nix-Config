20ccfcbad03657eea31769dcc672b4335944885c
Danz <57308957+D4NZ-jpg@users.noreply.github.com>
2025-11-04 20:43:14 -0600

    fix(git): migrate to new settings format and add common ignores
    - Migrate deprecated git options (userName, userEmail, extraConfig) to settings format
    - Add common ignores: node_modules, .venv, Python cache files, IDE directories, .env files
    - Add useful git settings: autocrlf, color.ui, push.default, pull.rebase, rebase.autoStash, diff.colorMoved

--- home/git.nix
+++ home/git.nix
@@ -2,20 +2,50 @@
 {
   programs.git = {
     enable = true;
-    userName = "Danz";
-    userEmail = "57308957+D4NZ-jpg@users.noreply.github.com";
-
-    lfs.enable = true;
-
-    ignores = [ "**/.DS_STORE" ];
-
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Danz";
+        email = "57308957+D4NZ-jpg@users.noreply.github.com";
+      };
       github = {
         user = primaryUser;
       };
       init = {
         defaultBranch = "main";
       };
+      core = {
+        autocrlf = "input";
+        excludesfile = "~/.gitignore_global";
+      };
+      color = {
+        ui = true;
+      };
+      push = {
+        default = "simple";
+      };
+      pull = {
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
+      };
+      diff = {
+        colorMoved = "default";
+      };
     };
+
+    lfs.enable = true;
+
+    ignores = [
+      "**/.DS_STORE"
+      "**/node_modules"
+      "**/.venv"
+      "**/__pycache__"
+      "**/*.pyc"
+      "**/.pytest_cache"
+      "**/.mypy_cache"
+      "**/.idea"
+      "**/.vscode"
+    ];
   };
 }
